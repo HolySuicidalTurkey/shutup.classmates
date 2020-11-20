72212d59e8577650fbb7978d7b6018d8635adcc7
HolySuicidalTurkey <63883308+HolySuicidalTurkey@users.noreply.github.com>
2020-11-20 11:43:42 -0600

    Create shutup.saige.cs
    shut up shut up shut up
    
    i have no brain REEEE

---
+++ shutup.saige.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine; //lulz wat why Unitee
+using Classmates;
+
+public class shutupsaige
+{
+void Start
+{
+function Classmates.mute = "Shutup"
+}
+  void Update
+  {
+  Classmates.Shutup("Saige")
+  Console.log("Shut up shut up shut up shut up")
+  }
+ }
